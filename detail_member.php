064a16dac5962c417ca8ad95fa93e1d10eb47e19
Myrrh5 <109903258+Myrrh5@users.noreply.github.com>
2022-10-23 17:50:36 +0700

    Add files via upload

---
+++ detail_member.php
@@ -0,0 +1,37 @@
+<?php
+$keyword = $_GET["keyword"];
+$conn = mysqli_connect("localhost","root","");
+if($con == ''){
+    mysqli_select_db($conn,"blueshop");
+}else{
+    echo mysqli_errno();
+}
+$sql = "SELECT * FROM member WHERE name LIKE '%$keyword%'";
+$objQuery = mysqli_query($conn,$sql);
+?>
+<table border="1">
+    <?php
+    while($row = mysqli_fetch_array($objQuery)){
+        ?>
+        <tr>
+            <td>
+                <?=$row["username"]?>
+            </td>
+            <td>
+                <?=$row["password"]?>
+            </td>
+            <td>
+                <?=$row["name"]?>
+            </td>
+            <td>
+                <?=$row["address"]?>
+            </td>
+            <td>
+                <?=$row["mobile"]?>
+            </td>
+            <td>
+                <?=$row["email"]?>
+            </td>
+        </tr>
+        <?php } ?>
+</table>
